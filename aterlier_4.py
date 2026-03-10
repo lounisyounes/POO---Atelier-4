5ff799c744382b83f5b43c660e0ac1e5889eac00
lounisyounes <youneslounis887@gmail.com>
2026-03-10 13:53:48 -0400

    Implementation des methodes de la classe Employe

--- aterlier_4.py
+++ aterlier_4.py
@@ -1,5 +1,16 @@
 class Employe:
-    def __init__(self, id, nom):
-        self.id = id
+    def __init__(self, numeroPermis, nom, prenom):
+        self.numeroPermis = numeroPermis
         self.nom = nom
-        self.voiture = None
+        self.prenom = prenom
+        self.voitureService = None
+    def afficherInformations(self):
+        return (self.numeroPermis, self.nom, self.prenom, self.voitureService)
+    def affecterVoiture(self, voiture):
+        if self.voitureService is None and voiture.chauffeur is None:
+            self.voitureService = voiture
+            voiture.chauffeur = self
+    def retirerVoiture(self):
+        if self.voitureService is not None:
+            self.voitureService.chauffeur = None
+            self.voitureService = None
